b759701560f7c224b08b4beafbc6c429a2517a8e
tony3sord <tressordtony@gmail.com>
2024-10-03 12:01:57 -0400

    integrando repository providers

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -3,10 +3,11 @@ import { ProvidersController } from './controllers/providers.controller';
 import { ProvidersService } from './services/providers.service';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsuarioModule } from 'src/usuario/usuario.module';
+import { ProvidersRepository } from './repository/providers.repository';
 
 @Module({
   controllers: [ProvidersController],
-  providers: [ProvidersService],
+  providers: [ProvidersService, ProvidersRepository],
   imports: [AuthModule, UsuarioModule],
 })
 export class ProvidersModule {}
