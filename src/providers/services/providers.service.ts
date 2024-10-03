ecbcf1256c4e187d4b440f76e90d2fd5197cd56a
tony3sord <tressordtony@gmail.com>
2024-10-03 12:07:15 -0400

    integrando con servicios providers

--- src/providers/services/providers.service.ts
+++ src/providers/services/providers.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProvidersDto } from '../dto/createproviders.dto';
+import { ProvidersRepository } from '../repository/providers.repository';
+import { Providers } from '../entity/providers.entity';
 
 @Injectable()
 export class ProvidersService {
-  async createProviders(usuario: CreateProvidersDto) {
-    return 0;
+  constructor(private readonly providersRepository: ProvidersRepository) {}
+
+  async createProviders(providers: CreateProvidersDto): Promise<Providers> {
+    return await this.providersRepository.createProviders(providers);
   }
-  async getProviders() {
-    return 0;
+
+  async getProviders(): Promise<Providers[]> {
+    return await this.providersRepository.getProviders();
   }
-  async getProvider(id: number) {
-    return 0;
+
+  async getProvider(id: number): Promise<Providers> {
+    return await this.providersRepository.getProvider(id);
   }
-  async deleteProvider(id: number) {
-    return 0;
+
+  async getProvidersForUser(user: string): Promise<Providers | boolean> {
+    return await this.providersRepository.getProvidersForUser(user);
+  }
+
+  async deleteProvider(id: number): Promise<String> {
+    return await this.providersRepository.deleteProvider(id);
   }
 }
